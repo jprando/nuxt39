fd1f91d49dbb67eb483054b2dd096c5869d356f9
Jeudi Prando <jeudiprando@gmail.com>
2023-12-31 22:08:02 -0300

    feature: netlify config teste

--- app.vue
+++ app.vue
@@ -1,20 +1,18 @@
 <template>
-  <ClientOnly>
-    <TesteInput v-model="teste" />
-    <pre>{{ teste }}</pre>
-    <div>
-      <label>
-        <div>titulo</div>
-        <input v-model="teste.titulo" />
-      </label>
-    </div>
-    <div>
-      <label>
-        <div>valor</div>
-        <input v-model="teste.valor" />
-      </label>
-    </div>
-  </ClientOnly>
+  <TesteInput v-model="teste" />
+  <pre>{{ teste }}</pre>
+  <div>
+    <label>
+      <div>titulo</div>
+      <input v-model="teste.titulo" />
+    </label>
+  </div>
+  <div>
+    <label>
+      <div>valor</div>
+      <input v-model="teste.valor" />
+    </label>
+  </div>
 </template>
 
 <script setup lang="ts">
